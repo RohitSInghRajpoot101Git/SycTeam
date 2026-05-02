717365d6f446a0c8b10b5d7efd1bca767037dbf9
rohit <rohit19801496@@gmail.com>
2026-05-02 19:21:09 +0530

    Created basic model structure Tested DB Connection

---
+++ backend/test_db.py
@@ -0,0 +1,8 @@
+from app.core.database import engine
+
+try:
+    connection = engine.connect()
+    print("✅ Database connected successfully")
+    connection.close()
+except Exception as e:
+    print("❌ Error:", e)
